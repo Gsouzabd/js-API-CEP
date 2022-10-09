bc5b2f3a49a0496a0aa3b61639500b67028ae592
gabrielsouza <gabrielsouzabd@outlook.com>
2022-10-09 08:43:25 -0300

    initial project

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+async function buscaEndereco(cep){
+  
+    
+    try{
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+
+        if (consultaCEPConvertida.erro){
+            throw Error('CEP Inválido!');
+        }
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    }catch(erro){
+        console.log(erro);
+    }
+}
+
+var campoCep = document.getElementById('cep');
+campoCep.addEventListener('focusout', () => buscaEndereco(campoCep.value))
+
+
+
+
